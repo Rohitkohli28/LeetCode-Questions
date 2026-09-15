81509fe7768a1f65c3c77fa08c02fa781acf121e
Rohit Kumar Kohli <145662458+Rohitkohli28@users.noreply.github.com>
2026-09-15 20:14:00 +0530

    Time: 152 ms (16.08%), Space: 62.2 MB (23.12%) - LeetHub

---
+++ 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int maxPalindromes(String s, int k) {
+        int n = s.length();
+
+        boolean[][] pal = new boolean[n][n];
+
+        // Build palindrome table
+        for (int len = 1; len <= n; len++) {
+            for (int i = 0; i + len <= n; i++) {
+                int j = i + len - 1;
+
+                if (len == 1) {
+                    pal[i][j] = true;
+                } else if (len == 2) {
+                    pal[i][j] = s.charAt(i) == s.charAt(j);
+                } else {
+                    pal[i][j] = s.charAt(i) == s.charAt(j)
+                            && pal[i + 1][j - 1];
+                }
+            }
+        }
+
+        int[] dp = new int[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            // Don't take a palindrome ending at i-1
+            dp[i] = dp[i - 1];
+
+            for (int j = 0; j < i; j++) {
+                int len = i - j;
+
+                if (len >= k && pal[j][i - 1]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+
+        return dp[n];
+    }
+}
